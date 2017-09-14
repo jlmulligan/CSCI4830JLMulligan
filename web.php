33408b4e4db26e53676cd851188a5c05f12f1717
jlmulligan <31715435+jlmulligan@users.noreply.github.com>
2017-09-13 22:34:43 -0500

    Update web.php

--- web.php
+++ web.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,6 +10,36 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 Route::get('/', function () {
-    return view('welcome');
+        $contacts=DB::table('contact_list')->get();
+//      $this->viewData['contact_list'] = $contacts;
+//      dd($this);    
+//      return view('welcome', $this->viewData);
+        return view('welcome', compact('contacts'));
+});
+
+Route::post('/addContact', function(Request $request) {
+        $cName=$request->input('NAME');
+        $cCom=$request->input('Company');
+        $cNum=$request->input('PreferredPhone');
+        $cMail=$request->input('Email');
+        $cAdd=$request->input('PostalAddress');
+        $cMeet=$request->input('MeetingTime');
+        $cID=$request->input('id');
+//      DB::table('contact_list')->insert('insert into contact_list (NAME, Company, PreferredPhone, Email, PostalAddress, MeetingTime, id) values (?,?,?,?,?,?,?)', 
+//              [$cName, $cCom, $cNum, $cMail, $cAdd, $cMeet, $cID]);
+        DB::table('contact_list')->insert([
+        ['NAME'=>$cName, 'Company'=>$cCom, 'PreferredPhone'=>$cNum, 'Email'=>$cMail, 'PostalAddress'=>$cAdd, 'MeetingTime'=>$cMeet, 'id'=>$cID]
+        ]);
+        return redirect('/');
+});
+
+Route::get('/removeContact', function(Request $request){
+//      dd($request2);
+        $cID=$request->input('id');
+        DB::table('contact_list')->where('id', '=', $cID)->delete();
+        return redirect('/');
 });
